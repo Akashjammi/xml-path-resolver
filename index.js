220fd290f1dbbc2fdd565472f5e294db0c5b398d
Akashjammi <jpakash05@gmail.com>
2022-02-13 09:56:48 +0530

    added test cases

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const convert = require("xml-js");
 const _ = require("lodash");
 /**
- *
- * @param {String} xml
- * @param {JSON} options
+ * @author AKASH J P ,Gokulnath
+ * @param {String} xml (Mandatory)
+ * @param {JSON} options (Optional)
  * @param options.crossReference {Regex} example x_(.*)
  */
 const xmlPathResolver = (xml, options = {}) => {
@@ -19,63 +19,71 @@ const xmlPathResolver = (xml, options = {}) => {
   }
 };
 function extractArrayPaths(obj, prefix, current, arrayPaths) {
-  prefix = prefix || [];
-  current = current || {};
-  arrayPaths = arrayPaths || {};
+  try {
+    prefix = prefix || [];
+    current = current || {};
+    arrayPaths = arrayPaths || {};
 
-  if (typeof (obj) === 'object' && obj !== null && !_.isArray(obj)) {
-    Object.keys(obj).forEach(key => {
-      extractArrayPaths(obj[key], prefix.concat(key), current, arrayPaths).current
-    });
-  }
-  else if (_.isArray(obj)) {
-    let lastPrefix = prefix[prefix.length-1];
-    if (arrayPaths[lastPrefix] && _.isArray(arrayPaths[lastPrefix])) {
-      arrayPaths[lastPrefix] = [...arrayPaths[lastPrefix], ...obj];
-    } else {
-      arrayPaths[lastPrefix] = obj;
+    if (typeof (obj) === 'object' && obj !== null && !_.isArray(obj)) {
+      Object.keys(obj).forEach(key => {
+        extractArrayPaths(obj[key], prefix.concat(key), current, arrayPaths).current
+      });
     }
-  }
-  else {
-    current[prefix.join('.')] = obj;
-  }
-  if(_.isObject(obj) && !_.isArray(obj)){
-    let lastPrefix = prefix[prefix.length-1];
-    if (arrayPaths[lastPrefix]) {
-      arrayPaths[lastPrefix].push(obj); 
-    } 
+    else if (_.isArray(obj)) {
+      let lastPrefix = prefix[prefix.length - 1];
+      if (arrayPaths[lastPrefix] && _.isArray(arrayPaths[lastPrefix])) {
+        arrayPaths[lastPrefix] = [...arrayPaths[lastPrefix], ...obj];
+      } else {
+        arrayPaths[lastPrefix] = obj;
+      }
+    }
     else {
-      arrayPaths[lastPrefix] = [obj];
+      current[prefix.join('.')] = obj;
     }
-  }
+    if (_.isObject(obj) && !_.isArray(obj)) {
+      let lastPrefix = prefix[prefix.length - 1];
+      if (arrayPaths[lastPrefix]) {
+        arrayPaths[lastPrefix].push(obj);
+      }
+      else {
+        arrayPaths[lastPrefix] = [obj];
+      }
+    }
 
-  return { current, arrayPaths };
+    return { current, arrayPaths };
+  } catch (err) {
+    throw err;
+  }
 }
 
 function resolveCrossRefs(jsonFormOfXml, options, refpaths) {
-  return JSON.parse(jsonFormOfXml, (key, value) => {
-    if (options.crossReference && options.crossReference instanceof RegExp) {
-      if (options.crossReference.test(key)) {
-        let matches = options.crossReference.exec(key);
-        if (_.isArray(matches) && matches.length >= 1) {
-          let path = matches[1];
-          try {
-            if (refpaths[path]) {
-              let extractedRefsValue = refpaths[path].filter((path) => {
-                return path._attributes.id === value;
-              })[0];
-              let resolvedRefs = resolveCrossRefs(JSON.stringify(extractedRefsValue), options, refpaths);
-              return resolvedRefs;
+  try {
+    return JSON.parse(jsonFormOfXml, (key, value) => {
+      if (options.crossReference && options.crossReference instanceof RegExp) {
+        if (options.crossReference.test(key)) {
+          let matches = options.crossReference.exec(key);
+          if (_.isArray(matches) && matches.length >= 1) {
+            let path = matches[1];
+            try {
+              if (refpaths[path]) {
+                let extractedRefsValue = refpaths[path].filter((path) => {
+                  return path._attributes.id === value;
+                })[0];
+                let resolvedRefs = resolveCrossRefs(JSON.stringify(extractedRefsValue), options, refpaths);
+                return resolvedRefs;
+              }
+            } catch (err) {
+              return value;
             }
-          } catch (err) {
-            return value;
           }
+          return value;
         }
         return value;
       }
       return value;
-    }
-    return value;
-  });
+    });
+  } catch (err) {
+    throw err;
+  }
 }
 module.exports = xmlPathResolver;
--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 const xmlPathResolver = require('./index');
 
-test('convert xml with cross reference', () => {
+test('convert xml with nested cross reference', () => {
     const xmlString = `
         <?xml version="1.0" encoding="utf-8"?>  
         <note id="1212"  importance="high" logged="true" x_note="23">
@@ -96,7 +96,88 @@ test('convert xml with cross reference', () => {
     expect(resolvedJSON).toMatchObject(expectedResponse);
 });
 
-test('convert xml without cross reference', () => {
+test('convert xml with single cross reference', () => {
+    const xmlString = `
+        <?xml version="1.0" encoding="utf-8"?>  
+        <note id="1212"  importance="high" logged="true">
+        <title>Happy</title>
+         <todo>Work</todo>
+         <todo>Play</todo>
+        </note>
+        <note id="23" importance="high" logged="true">
+        </note>
+        <note importance="high" logged="true">
+        </note>
+        <person x_note="1212">
+        </person>`;
+    const resolvedJSON = xmlPathResolver(xmlString, { crossReference: /x_(.*)/ });
+    let expectedResponse = {
+        "_declaration": {
+            "_attributes": {
+                "version": "1.0",
+                "encoding": "utf-8"
+            }
+        },
+        "note": [
+            {
+                "_attributes": {
+                    "id": "1212",
+                    "importance": "high",
+                    "logged": "true"
+                },
+                "title": {
+                    "_text": "Happy"
+                },
+                "todo": [
+                    {
+                        "_text": "Work"
+                    },
+                    {
+                        "_text": "Play"
+                    }
+                ]
+            },
+            {
+                "_attributes": {
+                    "id": "23",
+                    "importance": "high",
+                    "logged": "true"
+                }
+            },
+            {
+                "_attributes": {
+                    "importance": "high",
+                    "logged": "true"
+                }
+            }
+        ],
+        "person": {
+            "_attributes": {
+                "x_note": {
+                    "_attributes": {
+                        "id": "1212",
+                        "importance": "high",
+                        "logged": "true"
+                    },
+                    "title": {
+                        "_text": "Happy"
+                    },
+                    "todo": [
+                        {
+                            "_text": "Work"
+                        },
+                        {
+                            "_text": "Play"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    expect(resolvedJSON).toMatchObject(expectedResponse);
+});
+
+test('convert xml without cross reference but with crossRefenece option', () => {
     const xmlString = `
         <?xml version="1.0" encoding="utf-8"?>  
         <note id="1212"  importance="high" logged="true">
@@ -111,6 +192,69 @@ test('convert xml without cross reference', () => {
         <person id="1">
         </person>`;
     const resolvedJSON = xmlPathResolver(xmlString, { crossReference: /x_(.*)/ });
+    let expectedResponse = {
+        "_declaration": {
+            "_attributes": {
+                "version": "1.0",
+                "encoding": "utf-8"
+            }
+        },
+        "note": [
+            {
+                "_attributes": {
+                    "id": "1212",
+                    "importance": "high",
+                    "logged": "true",
+                },
+                "title": {
+                    "_text": "Happy"
+                },
+                "todo": [
+                    {
+                        "_text": "Work"
+                    },
+                    {
+                        "_text": "Play"
+                    }
+                ]
+            },
+            {
+                "_attributes": {
+                    "id": "23",
+                    "importance": "high",
+                    "logged": "true"
+                }
+            },
+            {
+                "_attributes": {
+                    "importance": "high",
+                    "logged": "true"
+                }
+            }
+        ],
+        "person": {
+            "_attributes": {
+            }
+        }
+    }
+    expect(resolvedJSON).toMatchObject(expectedResponse);
+});
+
+test('convert xml without cross reference but without crossRefenece option', () => {
+    const xmlString = `
+        <?xml version="1.0" encoding="utf-8"?>  
+        <note id="1212"  importance="high" logged="true">
+        <title>Happy</title>
+         <todo>Work</todo>
+         <todo>Play</todo>
+        </note>
+        <note id="23" importance="high" logged="true">
+        </note>
+        <note importance="high" logged="true">
+        </note>
+        <person id="1">
+        </person>`;
+    const resolvedJSON = xmlPathResolver(xmlString);
     let expectedResponse = {
         "_declaration": {
             "_attributes": {
@@ -177,4 +321,8 @@ test('Throw error for invalid xml', () => {
         `;
 
     expect(() => xmlPathResolver(xmlString, { crossReference: /x_(.*)/ })).toThrow();
+});
+
+test('Throw error for no xml', () => { 
+    expect(() => xmlPathResolver().toThrow());
 });
